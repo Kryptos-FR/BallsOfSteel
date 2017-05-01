037b59eb5689219e69c38f3cf1d8f6596a6e8fbd
Baptiste Prevost <g-ba-prevost@gametools.jp>
2017-05-01 14:05:43 +0900

    [Network]Added network test scene

---
+++ BallsOfSteel/BallsOfSteel.Game/Client.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine;
+
+namespace BallsOfSteel
+{
+    public class Client : AsyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override async Task Execute()
+        {
+            while(Game.IsRunning)
+            {
+                // Do stuff every new frame
+                await Script.NextFrame();
+            }
+        }
+    }
+}

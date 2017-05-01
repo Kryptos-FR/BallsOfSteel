17e375cc56b7c63ccb5e41d4562d52ea85b62c88
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2017-05-01 19:47:44 +0900

    Update input controller

--- BallsOfSteel/BallsOfSteel.Game/UI/ControllerScript.cs
+++ BallsOfSteel/BallsOfSteel.Game/UI/ControllerScript.cs
@@ -1,15 +1,11 @@
-﻿// Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
-// See LICENSE.md for full license information.
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using SiliconStudio.Core.Annotations;
 using SiliconStudio.Core.Mathematics;
 using SiliconStudio.Xenko.Input;
 using SiliconStudio.Xenko.Engine;
 using SiliconStudio.Xenko.UI;
+using SiliconStudio.Xenko.UI.Controls;
+using SiliconStudio.Xenko.UI.Events;
 using SiliconStudio.Xenko.UI.Panels;
 
 namespace BallsOfSteel.UI
@@ -20,7 +16,8 @@ namespace BallsOfSteel.UI
 
         private Thumb leftThumb;
         private Thumb rightThumb;
-        private Vector2 mousePosition;
+        private Button attackButton;
+        private Button jumpButton;
 
         public override void Start()
         {
@@ -31,6 +28,8 @@ namespace BallsOfSteel.UI
             UI = UI ?? Entity.Get<UIComponent>();
 
             var page = UI.Page;
+            attackButton = page.RootElement.FindVisualChildOfType<Button>("AttackButton");
+            jumpButton = page.RootElement.FindVisualChildOfType<Button>("JumpButton");
             leftThumb = new Thumb(page.RootElement.FindVisualChildOfType<UIElement>("Lthumb"), deviceSize, UI.Resolution);
             rightThumb = new Thumb(page.RootElement.FindVisualChildOfType<UIElement>("Rthumb"), deviceSize, UI.Resolution);
         }
@@ -43,22 +42,30 @@ namespace BallsOfSteel.UI
             {
                 foreach (var pointerEvent in Input.PointerEvents)
                 {
+                    var position = pointerEvent.Position;
                     switch (pointerEvent.State)
                     {
                         case PointerState.Down:
-                            // TODO
-                            break;
-                        case PointerState.Move:
-                            var position = pointerEvent.Position;
                             if (position.X < 0.5f)
-                                leftThumb.Move(pointerEvent.Position);
+                                leftThumb.Down(position);
                             else
-                                rightThumb.Move(pointerEvent.Position);
-                            // TODO
+                                rightThumb.Down(position);
                             break;
+
+                        case PointerState.Move:
+                            if (position.X < 0.5f)
+                                leftThumb.Move(position);
+                            else
+                                rightThumb.Move(position);
+                            break;
+
                         case PointerState.Up:
-                            // TODO
+                            if (position.X < 0.5f)
+                                leftThumb.Up(position);
+                            else
+                                rightThumb.Up(position);
                             break;
+
                         case PointerState.Out:
                         case PointerState.Cancel:
                             break;
@@ -69,40 +76,91 @@ namespace BallsOfSteel.UI
             }
         }
 
+        /// <inheritdoc />
+        public override void Cancel()
+        {
+            if (attackButton != null)
+                attackButton.Click -= OnAttackButonClicked;
+            if (jumpButton != null)
+                jumpButton.Click -= OnJumpButonClicked;
+        }
+
+        private void OnAttackButonClicked(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void OnJumpButonClicked(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
 
     }
 
     internal class Thumb
     {
         private readonly Grid parentGrid;
-        private readonly Vector3 parentCenter;
+        private readonly Vector2 parentHalfSize;
         private readonly UIElement thumbControl;
-        private readonly Vector3 thumbCenter;
+        private readonly Vector2 thumbCenter;
         private readonly Vector2 deviceSize;
-        private readonly Vector3 uiResolution;
+        private readonly Vector2 uiResolution;
 
-        private bool isPressed;
+        private bool isDown;
 
         public Thumb([NotNull] UIElement thumbControl, Vector2 deviceSize, Vector3 uiResolution)
         {
             this.deviceSize = deviceSize;
-            this.uiResolution = uiResolution;
+            this.uiResolution = uiResolution.XY();
 
             this.thumbControl = thumbControl;
-            thumbCenter = new Vector3(thumbControl.Width, thumbControl.Height, 0) / 2;
+            thumbCenter = new Vector2(thumbControl.Width, thumbControl.Height) / 2;
             parentGrid = (Grid)thumbControl.Parent;
-            parentCenter = new Vector3(parentGrid.Width, parentGrid.Height, 0)/2;
+            parentHalfSize = new Vector2(parentGrid.Width, parentGrid.Height)/2;
+        }
+
+        public void Down(Vector2 screenPosition)
+        {
+            var uiPosition = uiResolution * screenPosition;
+            var parentPosition = parentGrid.WorldMatrix.TranslationVector.XY() + uiResolution / 2;
+
+            if ((uiPosition - parentPosition).LengthSquared() > parentHalfSize.X * parentHalfSize.X)
+                return;
+
+            isDown = true;
+            Move(screenPosition);
         }
 
         public void Move(Vector2 screenPosition)
         {
-            var uiPosition = uiResolution * new Vector3(screenPosition, 0);
-            var parentPosition = parentGrid.WorldMatrix.TranslationVector + uiResolution/2;
-            if (uiPosition.X < parentPosition.X - parentCenter.X || uiPosition.Y < parentPosition.Y - parentCenter.Y ||
-                uiPosition.X > parentPosition.X + parentCenter.X || uiPosition.Y > parentPosition.Y + parentCenter.Y)
+            if (!isDown)
                 return;
 
-            var absolutePosition = uiPosition - parentPosition + parentCenter - thumbCenter;
+            var uiPosition = uiResolution * screenPosition;
+            var parentPosition = parentGrid.WorldMatrix.TranslationVector.XY() + uiResolution/2;
+            var offset = uiPosition - parentPosition;
+            if (offset.LengthSquared() > parentHalfSize.X * parentHalfSize.X)
+            {
+                // Project on the outside circle
+                var direction = Vector2.Normalize(uiPosition - parentPosition);
+                offset = direction * parentHalfSize.X;
+            }
+
+            var absolutePosition = offset + parentHalfSize - thumbCenter;
+            var margin = thumbControl.Margin;
+            margin.Left = absolutePosition.X;
+            margin.Top = absolutePosition.Y;
+            thumbControl.Margin = margin;
+        }
+
+        public void Up(Vector2 screenPosition)
+        {
+            if (!isDown)
+                return;
+
+            isDown = false;
+            var absolutePosition = parentHalfSize - thumbCenter;
             var margin = thumbControl.Margin;
             margin.Left = absolutePosition.X;
             margin.Top = absolutePosition.Y;

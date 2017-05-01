b1de66f001687f30c814046bfe1c31b15af742af
Baptiste Prevost <g-ba-prevost@gametools.jp>
2017-05-01 14:38:17 +0900

    Added Server Script

--- BallsOfSteel/BallsOfSteel.Game/Server.cs
+++ BallsOfSteel/BallsOfSteel.Game/Server.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using SiliconStudio.Core.Mathematics;
@@ -10,34 +11,45 @@ using SiliconStudio.Xenko.Input;
 using SiliconStudio.Xenko.Engine;
 
 using System.Net.Sockets;
+using System.Threading;
 
 namespace BallsOfSteel
 {
-    public class Client : AsyncScript
+    public class Server : AsyncScript
     {
-        // Declared public member fields and properties will show in the game studio
-        private UdpClient client = new UdpClient();
+        struct Client
+        {
+            public long clientHash; // some kind of GUID or stuff?
+            public bool isAlive;
+            public string remoteIp;
+        }
 
-        public string hostIp = "127.0.0.1";
-        public int hostPort = 3748;
-        
+        public const int MaxPlayerCount = 64;
+        private Client[] clients = new Client[MaxPlayerCount];
+
+        // Declared public member fields and properties will show in the game studio
+        private UdpClient serverClient = new UdpClient();
+
         public override async Task Execute()
         {
             try
             {
-                client.Client.Connect(hostIp, hostPort);
-
-                client.Client.Send(new byte[] {0xFF});
+                serverClient.Client.Connect("127.0.0.1", 3748);
             }
             catch (Exception e)
             {
                 throw e;
             }
 
-            while (Game.IsRunning)
+            byte[] received = new byte[512];
+            while (true)
             {
-                // Do stuff every new frame
-                await Script.NextFrame();
+                serverClient.Client.Receive(received);
+
+                if (received[0] == 0xFF)
+                {
+                    throw new AbandonedMutexException();
+                }
             }
         }
     }
